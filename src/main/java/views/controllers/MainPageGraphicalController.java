a23be9d8af13a06f3a99b96d8078efe499ac66fd
Mahyar Afshinmehr <mahyarafshinmehr@gmail.com>
2022-05-27 11:40:56 +0430

    graphicSetup: put two buttons in main page menu

--- src/main/java/views/controllers/MainPageGraphicalController.java
+++ src/main/java/views/controllers/MainPageGraphicalController.java
@@ -1,4 +1,26 @@
 package views.controllers;
 
+import controllers.MainPageController;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import views.Main;
+
+import java.io.IOException;
+
 public class MainPageGraphicalController {
+
+    private MainPageController controller = MainPageController.getMainPageController();
+
+    @FXML
+    private VBox box;
+
+    public void logout(MouseEvent mouseEvent) throws IOException {
+        this.controller.logoutUser();
+        Main.loadFxmlFile("SecondPage");
+    }
+
+    public void profileMenu(MouseEvent mouseEvent) throws IOException {
+        Main.loadFxmlFile("ProfilePage");
+    }
 }
