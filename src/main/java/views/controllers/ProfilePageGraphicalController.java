e67b3ca56d3cb0d373750442dafb0655cb164c61
Mahyar Afshinmehr <mahyarafshinmehr@gmail.com>
2022-05-27 11:46:46 +0430

    graphicSetup: finish profile page template

--- src/main/java/views/controllers/ProfilePageGraphicalController.java
+++ src/main/java/views/controllers/ProfilePageGraphicalController.java
@@ -1,4 +1,38 @@
 package views.controllers;
 
+import controllers.ProfilePageController;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import views.Main;
+
+import java.io.IOException;
+
 public class ProfilePageGraphicalController {
+
+    private ProfilePageController controller = ProfilePageController.getProfilePageController();
+
+    @FXML
+    private VBox box;
+    @FXML
+    private ImageView profilePhoto;
+
+    @FXML
+    public void initialize(){
+
+    }
+
+    public void changeProfilePhoto(MouseEvent mouseEvent) {
+    }
+
+    public void changePassword(MouseEvent mouseEvent) {
+    }
+
+    public void changeNickname(MouseEvent mouseEvent) {
+    }
+
+    public void back(MouseEvent mouseEvent) throws IOException {
+        Main.loadFxmlFile("MainPage");
+    }
 }
