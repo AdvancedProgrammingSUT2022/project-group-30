fc49cfe4e35b84951a19516e23fd3b00afb86cff
Mahyar Afshinmehr <mahyarafshinmehr@gmail.com>
2022-05-27 10:41:13 +0430

    graphicSetup: create loginPage.fxml template

--- src/main/java/views/controllers/LoginPageGraphicalController.java
+++ src/main/java/views/controllers/LoginPageGraphicalController.java
@@ -1,4 +1,24 @@
 package views.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
 public class LoginPageGraphicalController {
+
+    @FXML
+    private VBox box;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+
+    @FXML
+    public void submit(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void back(MouseEvent mouseEvent) {
+    }
 }
