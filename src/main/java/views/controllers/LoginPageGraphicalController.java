4799039486428342e963452104ee1c01d99a8f7b
Mahyar Afshinmehr <mahyarafshinmehr@gmail.com>
2022-05-27 11:13:46 +0430

    graphicSetup: finish login menu

--- src/main/java/views/controllers/LoginPageGraphicalController.java
+++ src/main/java/views/controllers/LoginPageGraphicalController.java
@@ -1,11 +1,17 @@
 package views.controllers;
 
+import controllers.LoginPageController;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import views.Main;
+
+import java.io.IOException;
 
 public class LoginPageGraphicalController {
+
+    private LoginPageController controller = LoginPageController.getLoginPageController();
 
     @FXML
     private VBox box;
@@ -15,10 +21,19 @@ public class LoginPageGraphicalController {
     private TextField password;
 
     @FXML
-    public void submit(MouseEvent mouseEvent) {
+    public void submit(MouseEvent mouseEvent) throws IOException {
+        this.controller.setProgramDatabase();
+        if (this.controller.checkLoginErrors(username.getText(), password.getText())) {
+            RegisterPageGraphicalController.showPopup("Username and password didn't match!");
+            return;
+        }
+        this.controller.loginUser(username.getText());
+        RegisterPageGraphicalController.showPopup("user logged in successfully!");
+        Main.loadFxmlFile("MainPage");
     }
 
     @FXML
-    public void back(MouseEvent mouseEvent) {
+    public void back(MouseEvent mouseEvent) throws IOException {
+        Main.loadFxmlFile("SecondPage");
     }
 }
