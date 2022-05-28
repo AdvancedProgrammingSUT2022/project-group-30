5d4e669b367a2fd0edecf462fd31ca08e8d90f7c
Amir Kooshky <kooshkya@gmail.com>
2022-05-28 23:52:30 +0430

    ChatRoom: add gotochat button to start page

--- src/main/java/views/controllers/StartPageController.java
+++ src/main/java/views/controllers/StartPageController.java
@@ -6,7 +6,10 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import views.Main;
 import views.transitions.LoadingProgressBarAnimation;
+
+import java.io.IOException;
 
 public class StartPageController {
 
@@ -24,4 +27,10 @@ public class StartPageController {
         LoadingProgressBarAnimation animation = new LoadingProgressBarAnimation(loadingProgressBar);
         animation.play();
     }
+
+    @FXML
+    protected void goToChat() throws IOException {
+        Main.loadFxmlFile("ChatMainPage");
+    }
+
 }
