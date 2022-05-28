a04d17b230f0019376ae01e181744e9f9aaee76a
Amir Kooshky <kooshkya@gmail.com>
2022-05-29 03:10:09 +0430

    ChatRoom: add image to button

--- src/main/java/views/controllers/ChatMainPageController.java
+++ src/main/java/views/controllers/ChatMainPageController.java
@@ -1,11 +1,22 @@
 package views.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import views.Main;
 
 public class ChatMainPageController {
     @FXML
+    private Button backButton;
+
+    @FXML
     public void initialize() {
         Main.getStage().setWidth(800);
+        setImageToButton(backButton, "buttons/bg.png");
+    }
+
+    private void setImageToButton(Button button, String imageAddress) {
+        button.setGraphic(new ImageView(new Image(Main.class.getResource("/images/" + imageAddress).toString())));
     }
 }
