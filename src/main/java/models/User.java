a9c654385a65bd9fd3c98e64506f596d5639e411
Amir Kooshky <kooshkya@gmail.com>
2022-06-03 18:14:43 +0430

    ChatRoom: add id to users

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,9 +1,17 @@
 package models;
 
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class User {
+    private final int id;
     private String username;
     private String password;
     private String nickname;
@@ -13,6 +21,7 @@ public class User {
     private String lastScoreChangeTime;  // note : be careful of updating this field after each game and score change using updateUserLastScoreChangeTime() function in ProgramDatabase.java
 
     public User(String username, String password, String nickname, int score) {
+         this.id = getId();
         this.username = username;
         this.password = password;
         this.nickname = nickname;
@@ -56,27 +65,51 @@ public class User {
         return this.score;
     }
 
-    public void setImageName(String imageName){
+    public void setImageName(String imageName) {
         this.imageName = imageName;
     }
 
-    public String getImageName(){
+    public String getImageName() {
         return this.imageName;
     }
 
-    public void setLastLoginTime(String lastLoginTime){
+    public void setLastLoginTime(String lastLoginTime) {
         this.lastLoginTime = lastLoginTime;
     }
 
-    public String getLastLoginTime(){
+    public String getLastLoginTime() {
         return this.lastLoginTime;
     }
 
-    public void setLastScoreChangeTime(String lastScoreChangeTime){
+    public void setLastScoreChangeTime(String lastScoreChangeTime) {
         this.lastScoreChangeTime = lastScoreChangeTime;
     }
 
-    public String getLastScoreChangeTime(){
+    public String getLastScoreChangeTime() {
         return this.lastScoreChangeTime;
+    }
+
+    private static int getId() {
+        int nextId = 0;
+
+        try {
+            String input = new String(Files.readAllBytes(Paths.get("src", "main", "resources", "json", "userid.txt")));
+            if (!input.isEmpty()) {
+                nextId = Integer.parseInt(input);
+            }
+        } catch (IOException e) {}
+
+        File main = new File("src", "main");
+        File resources = new File(main, "resources");
+        File json = new File(resources, "json");
+        File useridFile = new File(json, "userid.txt");
+        try {
+            FileWriter userWriter = new FileWriter(useridFile);
+            userWriter.write(String.valueOf(nextId + 1));
+            userWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return nextId;
     }
 }
