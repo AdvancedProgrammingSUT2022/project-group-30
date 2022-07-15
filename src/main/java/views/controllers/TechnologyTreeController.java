a3aed243875df2d1ba995cfd8ceeade9fa986c38
Amir Kooshky <kooshkya@gmail.com>
2022-07-15 19:35:15 +0430

    TechnologyGraphics: make tech tree page's search button work

--- src/main/java/views/controllers/TechnologyTreeController.java
+++ src/main/java/views/controllers/TechnologyTreeController.java
@@ -3,6 +3,7 @@ package views.controllers;
 import controllers.GameController;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -21,6 +22,10 @@ public class TechnologyTreeController {
     private BorderPane topBar;
     @FXML
     private Pane techPane;
+    @FXML
+    private TextField searchField;
+
+    private ScrollPane scrollPane;
 
 
     private final int padding = 15;
@@ -33,7 +38,7 @@ public class TechnologyTreeController {
     public void initialize() {
         topBar.setStyle("-fx-border-color: blue");
 
-        ScrollPane scrollPane = new ScrollPane();
+        scrollPane = new ScrollPane();
         scrollPane.setContent(techPane);
         scrollPane.setPannable(true);
         scrollPane.setFitToHeight(true);
@@ -82,6 +87,19 @@ public class TechnologyTreeController {
                     line.setStroke(Color.rgb(0, 0, 255, 0.5));
                 }
             }
+        }
+    }
+
+    @FXML
+    private void onSearchButtonPressed() {
+        String searchText = searchField.getText();
+        Technology technology = Technology.getTechnologyByName(searchText);
+        if (technology != null) {
+            TechnologyBlock block = techBlocks.get(technology);
+            double width = scrollPane.getContent().getBoundsInLocal().getWidth();
+            double x = block.getBoundsInParent().getMaxX();
+            scrollPane.setHvalue(x/width);
+            block.requestFocus();
         }
     }
 }
