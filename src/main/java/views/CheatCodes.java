c9d2d86f8c05d0d428c9b6f021e9dddf2312df6b
Amir Kooshky <kooshkya@gmail.com>
2022-07-17 12:12:52 +0430

    CheatBox: add main page cheat codes to cheatcodes enum

--- src/main/java/views/CheatCodes.java
+++ src/main/java/views/CheatCodes.java
@@ -6,7 +6,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum CheatCodes {
-    UNLOCK_TECHNOLOGY("alohomora (?<name>.*)");
+    UNLOCK_TECHNOLOGY("alohomora (?<name>.*)"),
+
+    // MAIN_PAGE
+    MAKE_EVERYTHING_VISIBLE("let there be light"),
+    ADD_GOLD("cash ziad eine hatami"),
+    DISABLE_TURN_BREAK("jenab sarvan welam kon"),
+    ADD_STRATEGIC_RESOURCE("need me some (?<name>.*)"),
+    ADD_LUXURY_RESOURCE("asan luxury shodan monshiam (?<name>.*)"),
+    ADD_UNIT("send dudes (?<name>.*)[-: ](?<y>\\d+)\\s*[,: ]\\s*(?<x>\\d+)"),
+    KILL_UNIT("avada kedavra (?<y>\\d+)\\s*[,: ]\\s*(?<x>\\d+)"),
+    MAKE_IMPROVEMENT("do you wanna build a snowman (?<name>.*) (?<y>\\d+)\\s*[:, ](?<x>\\d+)"),
+    DEPLOY_FEATURE("deploy feature (?<name>.*) (?<y>\\d+)\\s*[:, ](?<x>\\d+)"),
+    CLEAR_ALL_FEATURES("smite (?<y>\\d+)\\s*[:, ](?<x>\\d+)");
+
 
     private String regex;
 
