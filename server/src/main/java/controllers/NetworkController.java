49af75786869dfd63236818ad398f4be97af96be
Mahyar Afshinmehr <mahyarafshinmehr@gmail.com>
2022-07-21 01:55:07 +0430

    finish server client setup

--- server/src/main/java/controllers/NetworkController.java
+++ server/src/main/java/controllers/NetworkController.java
@@ -1,12 +1,20 @@
 package controllers;
 
+import com.google.gson.Gson;
+import models.GameMap;
+import models.ProgramDatabase;
+import models.User;
+import netPackets.Request;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 public class NetworkController {
 
@@ -27,11 +35,45 @@ public class NetworkController {
         return networkController;
     }
 
-    private String process(String request){
+    private String process(String data){
+        Request request = Request.fromJson(data);
+        try {
+            Method method = null/*= GameController.class.getMethod(request.getMethodName())*/;
+            Method[] methods = GameController.getGameController().getClass().getDeclaredMethods();
+            for(int i = 0; i < methods.length; i++){
+                if(methods[i].getName().equals(request.getMethodName())){
+                    method = methods[i];
+                    break;
+                }
+            }
+            ArrayList<String> arguments = request.getArguments();
+            Object[] parsedArguments = new Object[arguments.size()];
+            for (int i = 0; i < arguments.size(); i++) {
+                Gson gson = new Gson();
+                /*
+                * if... method.getParameterTypes()[i] Unit -> find the unit in server
+                 */
+                parsedArguments[i] = gson.fromJson(arguments.get(i), method.getParameterTypes()[i]);
+
+            }
+            Object result = method.invoke(GameController.getGameController(), (Object[]) parsedArguments);
+            Gson gson = new Gson();
+            return gson.toJson(result);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     public void run(){
+        User[] players = new User[2];
+        LoginPageController.getLoginPageController().setProgramDatabase();
+        players[0] = ProgramDatabase.getProgramDatabase().getUserByUsername("mahyarafshin");
+        players[1] = ProgramDatabase.getProgramDatabase().getUserByUsername("amir");
+        GameController.getGameController().addPlayers(players);
+        GameController.getGameController().initializeGame(20, 30, 0, 8);
         try {
             serverSocket = new ServerSocket(PORT_NUMBER);
             while (true) {
--- server/src/main/java/netPackets/Request.java
+++ server/src/main/java/netPackets/Request.java
@@ -1,4 +1,38 @@
 package netPackets;
 
+import com.google.gson.Gson;
+import models.GameMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Request {
+
+
+    private String methodName;
+    private ArrayList<String> arguments;
+
+    public Request(String methodName, String... arguments){
+        this.methodName = methodName;
+        this.arguments = new ArrayList<>(Arrays.asList(arguments));
+    }
+
+    public String toJson(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public static Request fromJson(String json){
+        Gson gson = new Gson();
+        return gson.fromJson(json, Request.class);
+    }
+
+    public String getMethodName(){
+        return this.methodName;
+    }
+
+    public ArrayList<String> getArguments(){
+        return this.arguments;
+    }
+
 }
